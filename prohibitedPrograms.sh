2afee532d8e451b31b9973b58e783286c204f673
Th3-0 <H31i05.h3110@protonmail.com>
2022-09-28 00:03:23 -0500

    Script for detecting programs(work in Progress)

--- prohibitedPrograms.sh
+++ prohibitedPrograms.sh
@@ -1,21 +1,46 @@
+#!/bin/bash -
+#title           :ProhibitedPrograms.sh
+#description     :Will use keywords to find hacking/prohibited programs(not malware)
+#author          :Th3-0
+#version         :0.1
+#usage           :"bash ProhibitedPrograms.sh" or "./ProhibitedPrograms.sh"
+#==========================================================================================
 echo "programs to look into deleting"
 echo "all output will be entered into investigate.txt file in home dir."
-touch investigate.txt
-dpkg -l | grep ftp >> investigate.txt
-dpkg -l | grep map >> investigate.txt
-dpkg -l | grep samba >> investigate.txt
-dpkg -l | grep snmp >> investigate.txt
-dpkg -l | grep telnet >> investigate.txt
-dpkg -l | grep tracer >> investigate.txt
-dpkg -l | grep trace >> investigate.txt
-dpkg -l | grep  >> investigate.txt
-dpkg -l | grep ripper >> investigate.txt
-dpkg -l | grep john >> investigate.txt
-dpkg -l | grep hydra >> investigate.txt
-dpkg -l | grep meta >> investigate.txt
-dpkg -l | grep sploit >> investigate.txt
-dpkg -l | grep nginx >> investigate.txt
-dpkg -l | grep dns >> investigate.txt
-dpkg -l | grep tftpd >> investigate.txt
-dpkg -l | grep nfs >> investigate.txt
-dpkg -l | grep vnc >> investigate.txt
+
+user=$(whoami)
+#insert thing to specify different keyword files
+read -p "enter the path of the keyword file:" KeywordsFile
+if [ $KeywordsFile =="" ];
+then
+   KeywordsFile="PPKeywords.txt"
+fi 
+
+mapfile Keywords < $KeywordsFile
+dpkg -l | awk '{print $2}' > /home/$user/keywords.temp
+for (( i=0; i<${#Keywords[@]}; i++ ));
+do #fix this clusterfuck at some point
+   grep ${Keywords[i]} /home/$user/keywords.temp > /home/$user/ProgramsToCheck #keep here as backup
+done
+clear
+end_question () {
+echo "Do you want to filter further? "
+select answer in "Yes" "No" "CheckCurrent"; do
+   case $answer in
+      Yes ) break;;
+      No ) echo "Exiting..."; exit;;
+      CheckCurrent ) 
+      clear;
+      echo "========================="; 
+      cat /home/$user/ProgramsToCheck; 
+      echo "========================="; 
+      end_question;;
+   esac
+done
+}
+end_question
+
+read -p "Enter other filters in {filter}|{filter} format(do not includ {}) COMMON EX: lib|driver:" SecondaryFilters
+#really fucking stupid-ill fix eventually
+grep -Ev $SecondaryFilters /home/$user/ProgramsToCheck > ProgramsToCheck1
+rm ProgramsToCheck |  && mv ProgramsToCheck1 ProgramsToCheck
